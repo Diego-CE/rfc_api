d8a53b1567fb00aced0e08c45b77068072d53d73
Diego Cadena <diego.c.e@hotmail.com>
2021-11-24 15:25:33 -0600

    Change way to initialize Rfc for each validation

--- app/models/rfc.rb
+++ app/models/rfc.rb
@@ -1,3 +1,14 @@
 class Rfc < ApplicationRecord
   attr_accessor :name, :last_name, :second_last_name, :birthdate
+
+  # Presence validations
+  validates :name,              presence: true
+  validates :last_name,         presence: true
+  validates :second_last_name,  presence: true
+  validates :birthdate,         presence: true
+
+  # Format Validations
+  validates :name, format: { with: /\A[a-zA-Z]+\z/, message: 'only allow letters' }
+
+
 end
--- spec/models/rfc_spec.rb
+++ spec/models/rfc_spec.rb
@@ -4,54 +4,64 @@ RSpec.describe Rfc, type: :model do
 
   context "attributes validations" do
 
-    rfc = Rfc.new(name: 'Nancy', last_name: 'Chávez',
-      second_last_name: 'Vázquez', birthdate: '1990-05-29')
+    rfc_valid_attributes = {name: 'Nancy', last_name: 'Chávez',
+      second_last_name: 'Vázquez', birthdate: '1990-05-29'}
 
     it "have valid attributes" do
+      rfc = Rfc.new(rfc_valid_attributes)
       expect(rfc).to be_valid
     end
 
     it "have not name" do
+      rfc = Rfc.new(rfc_valid_attributes)
       rfc.name = ""
       expect(rfc).to_not be_valid
     end
 
     it "have a invalid name" do
+      rfc = Rfc.new(rfc_valid_attributes)
       rfc.name = "N4ncy*"
       expect(rfc).to_not be_valid
     end
 
     it "have not last_name" do
+      rfc = Rfc.new(rfc_valid_attributes)
       rfc.last_name = ""
       expect(rfc).to_not be_valid
     end
 
     it "have a invalid last_name" do
+      rfc = Rfc.new(rfc_valid_attributes)
       rfc.last_name = "Cháv3z*"
       expect(rfc).to_not be_valid
     end
 
     it "have not second_last_name" do
+      rfc = Rfc.new(rfc_valid_attributes)
       rfc.second_last_name = ""
       expect(rfc).to_not be_valid
     end
 
     it "have a invalid last_name" do
+      rfc = Rfc.new(rfc_valid_attributes)
       rfc.second_last_name = "Vázqu3z*"
       expect(rfc).to_not be_valid
     end
 
     it "have not birthdate" do
+      rfc = Rfc.new(rfc_valid_attributes)
       rfc.birthdate = ""
       expect(rfc).to_not be_valid
     end
 
     it "have a invalid date" do
+      rfc = Rfc.new(rfc_valid_attributes)
       rfc.birthdate = "1990-05-32"
       expect(rfc).to_not be valid
     end
 
     it "have a invalid date format" do
+      rfc = Rfc.new(rfc_valid_attributes)
       rfc.birthdate = "1990/05/29"
       expect(rfc).to_not be_valid
     end
