cc140d07a0b134a0bfea4bba2f836c18d721eecd
Diego Cadena <diego.c.e@hotmail.com>
2021-11-24 18:06:21 -0600

    Add RFCs controller and write test for asking a key

---
+++ spec/requests/rfcs_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Rfcs Controller", type: :request do
+  describe "GET /new" do
+
+    let :req_params do
+      {
+        "name": "Nancy",
+        "last_name": "Chávez",
+        "second_last_name": "Vázquez",
+        "birthdate": "1990-05-29"
+      }
+    end
+
+    it "build new RFC key" do
+      get new_rfc_path, params: req_params
+      json = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(json).to eq({
+          rfc: "CAVN900529",
+          count: 1
+        })
+    end
+  end
+end
